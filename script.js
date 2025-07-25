48049850974d9002cdf1a3d10dd33654a327b6ed
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-25 11:15:39 +0530

    ch

--- script.js
+++ script.js
@@ -6,3 +6,19 @@
 
 document.getElementById("btn").textContent="changed"
 
+
+//function creating
+
+function print(){
+    let sum = 10 + 15;
+    console.log(sum)
+    console.log(1)
+
+    console.log(2)
+    console.log("HI I AM RAJ")
+
+}
+
+//calling fucntion
+
+print()
