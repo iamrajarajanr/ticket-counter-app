16f87eba502922284015166c6847cda33883121c
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-25 10:55:54 +0530

    changed

---
+++ basics.js
@@ -0,0 +1,50 @@
+console.log("welcome to JS");
+
+let firstName ="Raja";
+let secondName = "rajan";
+let lastName = " R"
+
+let full_name = firstName+secondName+lastName;
+console.log(full_name);
+
+
+let num1 = 12;
+let num2 = 12;
+
+let add = num1+num2;
+let sub = num1-num2;
+let div = num1/num2;
+let mul = num1*num2;
+let mod = num1%num2;
+
+console.log("The Answer is",add)
+console.log("The Answer is",sub)
+console.log("The Answer is",div)
+console.log("The Answer is",mul)
+console.log("The Answer is",mod)
+
+
+let nam ="raj"
+console.log(nam)
+
+console.log(typeof 12)
+console.log(typeof "12")
+
+console.log("THe sum is " + add + sub)
+
+//---------------------------------
+
+let thestring ="12"
+let thenumber = 12
+console.log(typeof thestring + thenumber)
+
+//--------------------------------
+
+let score=0
+console.log(score)
+
+// score updated to 3
+
+score=3
+console.log(score)
+
