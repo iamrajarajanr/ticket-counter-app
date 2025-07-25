0faee0158ca9a034ca1d19d7d0cc28221fdae290
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-25 10:12:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("welcome to JS");
+
+let firstName ="Raja";
+let secondName = "rajan";
+let lastName = " R"
+
+let full_name = firstName+secondName+lastName;
+console.log(full_name);
+
+
+let num1 = 12;
+let num2 = 12;
+
+let add = num1+num2;
+let sub = num1-num2;
+let div = num1/num2;
+let mul = num1*num2;
+let mod = num1%num2;
+
+console.log("The Answer is",add)
+console.log("The Answer is",sub)
+console.log("The Answer is",div)
+console.log("The Answer is",mul)
+console.log("The Answer is",mod)
