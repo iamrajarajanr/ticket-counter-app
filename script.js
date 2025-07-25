2080d13ca6a019f5119689c778b27fac93b666f5
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-25 11:06:49 +0530

    btn

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let button = document.getElementById("btn")
-console.log(button)
+// let button = document.getElementById("btn")
+// console.log(button)
+// button.textContent="hello"
+
+            //    (or)
+
+document.getElementById("btn").textContent="changed"
+
