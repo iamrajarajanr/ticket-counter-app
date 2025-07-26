1562ae37a8f3c525aca48a7428bcda3a6874aa4d
iamrajarajanr <iamrajarajanr@gmail.com>
2025-07-26 09:52:35 +0530

    cal

--- calculator-task/script.js
+++ calculator-task/script.js
@@ -1,7 +1,38 @@
-let num1 = 8
-let num2 = 2
+let num1 = 5
+let num2 = 10
+let num3 = 0
+
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+let out = document.getElementById("sum-el")
+
+
+let add = num1 + num2
+let sub = num1 - num2
+let div = num1 / num2
+let mul = num1 * num2
+
+
+
+function addition() {
+    out.textContent = "Sum: " + add;
+}
+
+function subtraction() {
+    out.textContent = "Sum: " + sub;
+}
+
+function division() {
+    out.textContent = "Sum: " + div;
+}
+
+function multiplication() {
+    out.textContent = "Sum: " + mul;
+}
+
+
+
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
