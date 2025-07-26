31cf1dd2185699242b472958d549d2902d5948d9
iamrajarajanr <iamrajarajanr@gmail.com>
2025-07-26 10:48:17 +0530

    finished

--- render-error-msg/index.js
+++ render-error-msg/index.js
@@ -1,12 +1,5 @@
 let message = document.getElementById("error")
 
 function purchase(){
-
     message.textContent = message.textContent + "⚠ This product is out of stock" 
-    
-    
-
-
-
-removed
 }
